037bc2b7bcf87cd42bc5b379298446992c1c31a4
yahoneogul <yahoneogul.gmail.com>
2021-01-01 06:27:04 +0900

    app.js 수정 clone 추가//goToPrev 수정 필요

--- app.js
+++ app.js
@@ -1,45 +1,74 @@
 (function () {
   const slider = document.querySelector(".slider");
-  const sliderWrapper = slider.querySelector(".slider-image-wrapper");
-  const sliderItem = slider.querySelectorAll(".slider-item");
+  let sliderItem = slider.querySelectorAll(".slider-item");
   const sliderBtns = slider.querySelector(".btns");
+  const sliderWrapper = document.querySelector(".slider-item-wrapper");
 
-  let sliderCount = sliderItem.length;
   let currentIdx = 0;
+  let sliderLength = sliderItem.length;
 
-  for (let i = 0; i < sliderCount; i++) {
+  //slider item clone
+  let firstChild = sliderWrapper.firstElementChild;
+  let lastChild = sliderWrapper.lastElementChild;
+  let clonedFirst = firstChild.cloneNode(true);
+  let clonedLast = lastChild.cloneNode(true);
+
+  sliderWrapper.appendChild(clonedFirst);
+  sliderWrapper.insertBefore(clonedLast, sliderWrapper.firstElementChild);
+
+  //변수 재할당
+  sliderItem = slider.querySelectorAll(".slider-item");
+  sliderLength = sliderItem.length;
+
+  //수평 정렬
+  for (let i = 0; i < sliderLength; i++) {
     sliderItem[i].style.left = i * 100 + "%";
   }
+  sliderWrapper.style.transform = "translate3D(-100%, 0, 0)";
 
-  function goToSlide(idxNum) {
-    sliderWrapper.style.left = -100 * idxNum + "%";
-    sliderWrapper.classList.add("move-slider");
-    currentIdx = idxNum;
+  function goToSlider() {
+    sliderWrapper.style.transform =
+      "translate3D(" + -100 * (currentIdx + 1) + "%, 0, 0)";
+    sliderWrapper.classList.add("animate-slider");
   }
 
-  function goToPrev(currentIdx) {
-    if (currentIdx == 0) {
-      goToSlide(sliderCount - 1);
-    } else {
-      goToSlide(currentIdx - 1);
+  function goToPrev() {
+    if (currentIdx > 0) {
+      currentIdx--;
+      goToSlider();
+    }
+    if (currentIdx === 0) {
+      setTimeout(function () {
+        currentIdx = 5;
+        sliderWrapper.style.transform = "translate3D(-600%, 0, 0)";
+        sliderWrapper.classList.remove("animate-slider");
+      }, 301);
+      console.log(currentIdx);
     }
   }
-
-  function goToNext(currentIdx) {
-    if (currentIdx == sliderCount - 1) {
-      goToSlide(0);
-    } else {
-      goToSlide(currentIdx + 1);
+  function goToNext() {
+    if (currentIdx < sliderLength - 2) {
+      currentIdx++;
+      goToSlider();
+    }
+    if (currentIdx === sliderLength - 2) {
+      setTimeout(function () {
+        currentIdx = 0;
+        sliderWrapper.style.transform = "translate3D(-100%, 0, 0)";
+        sliderWrapper.classList.remove("animate-slider");
+      }, 301);
     }
   }
 
   function btnsClickHandler(e) {
     const targetBtn = e.target;
+
     if (targetBtn.classList.contains("btn-prev")) {
-      goToPrev(currentIdx);
+      goToPrev();
+      console.log(currentIdx, sliderLength - 2);
     }
     if (targetBtn.classList.contains("btn-next")) {
-      goToNext(currentIdx);
+      goToNext();
     }
   }
 
